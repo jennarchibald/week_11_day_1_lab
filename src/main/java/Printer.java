506ade63486e493c867da8098ec7525b6191ec61
jennarchibald <jennarchibald91@gmail.com>
2019-05-06 19:51:38 +0100

    adding private methods

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -12,11 +12,30 @@ public class Printer {
     }
 
     public void print(int numberOfPages, int numberOfCopies) {
+
         int sheetsToBePrinted = numberOfCopies * numberOfPages;
-        if (sheetsToBePrinted <= remainingSheets && sheetsToBePrinted <= tonerVolume){
-            remainingSheets -= sheetsToBePrinted;
-            tonerVolume -= sheetsToBePrinted;
+
+        if (this.enoughSheets(sheetsToBePrinted) && this.enoughToner(sheetsToBePrinted)){
+
+            this.useSheets(sheetsToBePrinted);
+            this.useToner(sheetsToBePrinted);
         }
+    }
+
+    private void useSheets(int usedSheets){
+        remainingSheets -= usedSheets;
+    }
+
+    private void useToner(int tonerUsed){
+        tonerVolume -= tonerUsed;
+    }
+
+    private boolean enoughSheets(int requiredSheets){
+        return requiredSheets <= remainingSheets;
+    }
+
+    private boolean enoughToner(int requiredToner){
+        return requiredToner <= tonerVolume;
     }
 
     public void refill(int newSheets) {
